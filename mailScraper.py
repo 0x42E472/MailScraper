506e2a27cedb35643d503bd126f7505d1ab8ddab
okcomputer <ok>
2017-08-23 23:21:20 -0400

    Added writing to file. Going to work on adding arguments now.

--- mailScraper.py
+++ mailScraper.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import requests
 import ssl
 from bs4 import BeautifulSoup
@@ -8,12 +7,15 @@ import sys
 
 
 class ScrapeEmails():
-    """testing"""
+    """Practicing some programming."""
     
     def __init__(self):
-        self.link = "put the link here"
+        """TODO: Change name to domain name of url, change url to accept file or arg"""
+        self.name = "test"
+        self.link = ""
 
     def check_href(self, content):
+        """Check for emails in links."""
         self.placeholder = "test"
         self.check_links = content.find_all('a', href=True)
 
@@ -23,49 +25,40 @@ class ScrapeEmails():
                 yield oh_hai[1]
 
     def check_text(self, content):
+        """Check the html for emails."""
         words = content.get_text().split()
         for word in words:
             if "@" in word:
                 yield word
 
     def dedup(self, link, text):
+        """Dedup the emails from both sources."""
         t = []
         s = []
         for i in link:
             t.append(i)
             for e in text:
                 s.append(e)
-        fuck = list(set(t) - set(s))
-        result = t + list(fuck)
+        email_list = list(set(t) - set(s))
+        result = t + list(email_list)
         return result
 
 
         
     def get_it(self):
+        """TODO: Separate this out to another function"""
         content = []
         r = requests.get(self.link)
         html_doc = r.content
         soup = BeautifulSoup(html_doc, 'html.parser')
         link_emails = self.check_href(soup)
         text_emails = self.check_text(soup)
-        herro = self.dedup(link_emails, text_emails)
-        print herro
+        the_goods = self.dedup(link_emails, text_emails)
+        f = open(self.name,"w+")
+        for i in the_goods:
+            f.write("{0}\n".format(i))
+        f.close()
 
-        sys.exit()
-
-        toText.close()
-        print("Saved: " + str(name) + ".txt")
-    
-        counter = 0
-        if len(sys.argv) > 1:
-            path = sys.argv[1] 
-        else:
-        
-            path = input("Path or URL:\n")
-    
-        if (path[0:4] == "http"):
-            get_it(path, counter)
 if __name__ == "__main__":
     se = ScrapeEmails()
     t = se.get_it()
-    print t
