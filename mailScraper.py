c3bcea1f2a342f090fb5e327bd10a38fb0d4c5d5
Axin <axin_all_cool@protonmail.com>
2017-08-24 19:41:37 -0500

    updated my git thingies

--- mailScraper.py
+++ mailScraper.py
@@ -1,64 +1,179 @@
 #!/usr/bin/env python
-import requests
-import ssl
-from bs4 import BeautifulSoup
-import random
+
+import re
 import sys
+import argparse
+import requests
+import Queue
+import threading
+import yaml
+import time
+import traceback
+
+#
+# TODO: This probably needs more error handling....maybe
+# TODO: The default config shouldn't be the URLS.yml test config
+#
 
 
+#
+# Class that scrapes a URL and find all the email addresses on the page
+# 
 class ScrapeEmails():
-    """Practicing some programming."""
+
+    def __init__(self, timeout=30):
+        self.timeout = timeout
+        self.email_regex = re.compile(r"([a-zA-Z0-9._%-]+@[a-zA-Z-9._%-]+\.[a-zA-Z]{2,6})")
+
+    def _get(self, url):
+        return requests.get(url, timeout=self.timeout, verify=True)
     
-    def __init__(self):
-        """TODO: Change name to domain name of url, change url to accept file or arg"""
-        self.name = "test"
-        self.link = ""
-
-    def check_href(self, content):
-        """Check for emails in links."""
-        self.placeholder = "test"
-        self.check_links = content.find_all('a', href=True)
-
-        for link in self.check_links:
-            if "@" in link['href']:
-                oh_hai =link['href'].split(':')
-                yield oh_hai[1]
-
-    def check_text(self, content):
-        """Check the html for emails."""
-        words = content.get_text().split()
-        for word in words:
-            if "@" in word:
-                yield word
-
-    def dedup(self, link, text):
-        """Dedup the emails from both sources."""
-        t = []
-        s = []
-        for i in link:
-            t.append(i)
-            for e in text:
-                s.append(e)
-        email_list = list(set(t) - set(s))
-        result = t + list(email_list)
-        return result
-
-
+    def scrape(self, url):
+        emails = []
+        response = self._get(url)
         
-    def get_it(self):
-        """TODO: Separate this out to another function"""
-        content = []
-        r = requests.get(self.link)
-        html_doc = r.content
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        link_emails = self.check_href(soup)
-        text_emails = self.check_text(soup)
-        the_goods = self.dedup(link_emails, text_emails)
-        f = open(self.name,"w+")
-        for i in the_goods:
-            f.write("{0}\n".format(i))
-        f.close()
+        if response.status_code == 200:
+            emails = self.email_regex.findall(response.text)
 
+        return emails
+        
+#
+# This is the worker, it calls the scrapper calss when it has work
+#
+class ThreadWorker(threading.Thread):
+
+    def __init__(self, thread_id, name, queue, lock):
+        threading.Thread.__init__(self)
+        self.thread_id = thread_id
+        self.name = name
+        self.queue = queue
+        self.lock = lock
+        self.scrapper = ScrapeEmails()
+
+    def run(self):
+        print "Starting {0}".format(self.name)
+
+        while not self.queue.empty():
+            self.lock.acquire()
+
+            if not self.queue.empty():
+                url = self.queue.get()
+                self.lock.release()
+
+                emails = self.scrapper.scrape(url)
+
+                if len(emails) > 0:
+                    for email in emails:
+                        print "{0}: {1} -- Found: [{2}]".format(self.name, url, email)
+                else:
+                    print "{0}: {1} -- No emails Found".format(self.name, url)
+
+            else:
+                self.lock.release()
+
+            time.sleep(1)
+
+        print "Stopping {0}".format(self.name)
+            
+
+#
+# Method to load a config yaml file
+#
+def load_yaml_config(filename):
+    data = None
+    
+    with open(filename, 'r') as stream:
+        try:
+            data = yaml.load(stream)
+        except yaml.YAMLError as e:
+            ValueError(e)
+            
+    return data        
+
+
+#
+# Closes all the threads
+#
+def close_all_threads(queue_lock, threads):
+    queue_lock.acquire()
+
+    queue = None
+
+    # Stop all threads
+    for t in threads:
+        t.join()
+
+    queue_lock.release()
+
+
+#
+# Fills the queue with URLs
+#
+def fill_queue(queue,lock,urls):
+    lock.acquire()
+    for url in urls:
+        queue.put(url)            
+    lock.release()
+
+
+#
+# Parses command line arguments
+#
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', default='URLS.yml', help='Yaml config file containing the URLs')
+    parser.add_argument('-T', '--timeout', default=30, help='Seconds to wait for a URL response')
+    parser.add_argument('-t', '--threads', default=1, help='Number of threads to create')
+
+    # Show usage if wedon't have what we need
+    if 'file' not in parser.parse_args():
+        parser.print_help()
+        
+    return parser.parse_args()
+
+
+#
+# Main - Puts it all together
+#
+def main():
+    # Get command line args
+    args = parse_args()
+
+    # Setup things that we need
+    lock = threading.Lock()
+    queue = Queue.Queue()
+    threads = []
+    stop_threads = False
+    
+    # Load config file
+    config = load_yaml_config(args.file)
+    
+    # fill the queue
+    fill_queue(queue, lock, config['urls'])
+
+    # Create the threads
+    for thread_id in range(1, int(args.threads) + 1):
+        name = "Thread-{0}".format(thread_id)
+        thread = ThreadWorker(thread_id, name, queue, lock)
+        thread.start()
+        threads.append(thread)
+
+    # Wait for the queue to empty
+    while not queue.empty():
+        pass        
+
+    # Stop all threads
+    close_all_threads(lock, threads)
+
+    print 'Exiting Main'
+    sys.exit(0)
+
+
+#
+# Only run main() if this file was executed directly  
+#
 if __name__ == "__main__":
-    se = ScrapeEmails()
-    t = se.get_it()
+    try:
+        main()
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
